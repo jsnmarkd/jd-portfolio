e7c05df8d2fad0bd35fd2299732f2130820c5133
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-09 18:48:44 -0400

    Implement float effect, add lighting with Environment helper, and move the laptop position

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,17 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF, Environment, Float } from "@react-three/drei";
 
 export default function Experience() {
   const computer = useGLTF("./laptop-model.gltf");
 
   return (
     <>
+      <Environment preset="city" />
       <color attach={"background"} args={["#241a1a"]} />
       <OrbitControls makeDefault />
 
-      <primitive object={computer.scene} />
+      <Float rotationIntensity={0.4}>
+        <primitive object={computer.scene} position-y={-1.2} />
+      </Float>
     </>
   );
 }
