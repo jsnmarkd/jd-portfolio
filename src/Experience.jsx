c72e761d66008ed451ddf8b8d5487fca354c3afa
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-10 17:24:39 -0400

    Add an iFrame to display website on the laptop model

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,6 +3,8 @@ import {
   useGLTF,
   Environment,
   Float,
+  ContactShadows,
+  Html,
 } from "@react-three/drei";
 
 export default function Experience() {
@@ -23,9 +25,21 @@ export default function Experience() {
         snap={{ mass: 4, tension: 400 }}
       >
         <Float rotationIntensity={0.4}>
-          <primitive object={computer.scene} position-y={-1.2} />
+          <primitive object={computer.scene} position-y={-1.2}>
+            <Html
+              wrapperClass="htmlScreen"
+              transform
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://bruno-simon.com" />
+            </Html>
+          </primitive>
         </Float>
       </PresentationControls>
+
+      <ContactShadows position-y={-1.4} />
     </>
   );
 }
