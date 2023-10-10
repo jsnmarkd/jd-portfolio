8a8cf433eaca4e60a41d1d6e2e3873f9f151ec39
Jason Divinagracia <jmarkd04@gmail.com>
2023-10-10 17:06:27 -0400

    Implement PresentationControls from Drei and configure the animation effects

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,9 @@
-import { OrbitControls, useGLTF, Environment, Float } from "@react-three/drei";
+import {
+  PresentationControls,
+  useGLTF,
+  Environment,
+  Float,
+} from "@react-three/drei";
 
 export default function Experience() {
   const computer = useGLTF("./laptop-model.gltf");
@@ -6,12 +11,21 @@ export default function Experience() {
   return (
     <>
       <Environment preset="city" />
-      <color attach={"background"} args={["#241a1a"]} />
-      <OrbitControls makeDefault />
 
-      <Float rotationIntensity={0.4}>
-        <primitive object={computer.scene} position-y={-1.2} />
-      </Float>
+      <color attach={"background"} args={["#241a1a"]} />
+
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <primitive object={computer.scene} position-y={-1.2} />
+        </Float>
+      </PresentationControls>
     </>
   );
 }
